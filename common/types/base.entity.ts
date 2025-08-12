5bbfe7fed6854c05e2649872da11064eb03db4a8
MoizAnsari7 <moizansari176@gmail.com>
2025-08-12 13:26:01 +0530

    base entity

---
+++ common/types/base.entity.ts
@@ -0,0 +1,14 @@
+import { CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+@Entity()
+export abstract class BaseEntity{
+    @PrimaryGeneratedColumn()
+    readonly id : number
+
+    @CreateDateColumn({type : 'timestamptz', default : ()=> 'CURRENT_TIMESTAMP'})
+    readonly createdAt : Date
+
+    @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+    readonly updateAt : Date
+}
